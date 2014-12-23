3b69c518c352e0a4e8e7da9c728cd95567fa32dd
adslyw <adslyw@gmail.com>
2014-12-23 16:32:20 +0800

    bug fixed

--- test.rb
+++ test.rb
@@ -4,4 +4,3 @@ require_relative 'config/bcv.rb'
 require_relative 'lib/worker.rb'
 connect(@bcv[:host],@bcv[:username],@bcv[:password],@bcv[:servicename])
 tableinfo "bf_account_fee_t"
-workout(@info_by_service_id,['15596376858','18609140195','18609149999'])
